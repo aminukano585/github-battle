ef514b204417bd7671eace90ae24b78920f786dd
AminuSufi585 <aminukano585@gmail.com>
2018-11-13 16:41:52 +0100

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Configuration is successful.. Let's code!!!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
